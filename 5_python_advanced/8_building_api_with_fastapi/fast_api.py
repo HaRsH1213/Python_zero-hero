457ab12cd58272720cf47024f1e0932ad5483790
Harsh Chauhan <78776708+HaRsH1213@users.noreply.github.com>
2025-09-05 21:40:10 +0530

    Add files via upload

---
+++ 5_python_advanced/8_building_api_with_fastapi/fast_api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from enum import Enum
+
+app= FastAPI()
+
+food_items = {
+    'indian' : [ "Samosa", "Dosa" ],
+    'american' : [ "Hot Dog", "Apple Pie"],
+    'italian' : [ "Ravioli", "Pizza"]
+}
+class AvailableCuisine(str,Enum):
+    indian ='indian'
+    american = 'american'
+    italian = 'italian'
+@app.get("/get_items/{cuisine}")
+def get_items(cuisine:AvailableCuisine):
+    return food_items.get(cuisine)
+
+
+coupon_code = {
+    1: '10%',
+    2: '20%',
+    3: '30%'
+}
+
+@app.get("/get_coupons/{code}")
+async def get_items(code : int):
+    return (f"Discount Amount : {coupon_code.get(code)} ")
